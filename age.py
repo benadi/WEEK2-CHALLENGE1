f594c5453a828098284c4de53db0d4657a113cbb
benadi <bennakiconco@gmail.com>
2019-02-25 07:11:09 -0800

    week 2 challenge 1

---
+++ age.py
@@ -0,0 +1,9 @@
+year_of_birth = input("enter year of birth")
+my_year = int(year_of_birth)
+age = 2019-my_year
+if age <18:
+	print("minor")
+elif age>=18 and age<=36:
+	print("youth")
+else:
+	print("elder")
---
+++ dictionary.py
@@ -0,0 +1,4 @@
+dictionari = {}
+for z in range(2,15):
+	dictionari[z] = z**2
+print(dictionari)
